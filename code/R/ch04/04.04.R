20ab8ba09b921546eaddbb0ab6f6250c94186cea
自分の名前 <jr.tr.ybk+github@gmail.com>
2022-10-25 21:18:36 +0900

    校正でカットした目のコードの削除

--- code/R/ch04/04.04.R
+++ code/R/ch04/04.04.R
@@ -149,27 +149,3 @@ hist(x = result, breaks = 0:15,
 quantile(result, c(0.025, 0.975))
 #>  2.5% 97.5%
 #>     0     5
-
-#### 4.4.5.2 平均の差の信頼区間
-
-X <- c(32.1, 26.2, 27.5, 31.8, 32.1, 31.2, 30.1, 32.4, 32.3, 29.9,
-       29.6, 26.6, 31.2, 30.9, 29.3)
-Y <- c(35.4, 34.6, 31.1, 32.4, 33.3, 34.7, 35.3, 34.3, 32.1, 28.3,
-       33.3, 30.5, 32.6, 33.3, 32.2)
-Z <- X - Y # 対標本として扱う．
-
-n <- 10^5
-result <- replicate(n, mean(sample(Z, length(Z), replace = TRUE)))
-
-quantile(result, c(0.025, 0.975))
-#>      2.5%     97.5%
-#> -3.880000 -1.566667
-
-hist(result)
-
-result <- replicate(n,
-                    mean(sample(X, length(X), replace = TRUE)) -
-                    mean(sample(Y, length(Y), replace = TRUE)))
-quantile(result, c(0.025, 0.975))
-#>      2.5%     97.5%
-#> -4.053333 -1.306667
--- code/py/ch04/04.04.py
+++ code/py/ch04/04.04.py
@@ -134,26 +134,3 @@ plt.hist(result, bins=range(0, 16))
 
 np.quantile(result, [0.025, 0.975])
 #> array([0., 5.])
-
-#### 4.4.5.2 平均の差の信頼区間
-
-import numpy as np
-X = [32.1, 26.2, 27.5, 31.8, 32.1, 31.2, 30.1, 32.4, 32.3, 29.9,
-     29.6, 26.6, 31.2, 30.9, 29.3]
-Y = [35.4, 34.6, 31.1, 32.4, 33.3, 34.7, 35.3, 34.3, 32.1, 28.3,
-     33.3, 30.5, 32.6, 33.3, 32.2]
-Z = np.array(X) - np.array(Y) # 対標本として扱う．
-
-n = 10**5
-result = [np.random.choice(Z, len(Z), replace=True).mean() for _ in range(n)]
-
-np.quantile(result, [0.025, 0.975])
-#> array([-3.88666667, -1.55333333])
-
-plt.hist(result, bins='sturges')
-
-result = [np.random.choice(X, len(X), replace=True).mean() -
-          np.random.choice(Y, len(Y), replace=True).mean()
-          for _ in range(n)]
-np.quantile(result, [0.025, 0.975])
-#> array([-4.06, -1.3 ])
